205eb9a70a67ea0f45219131fa3857955d792892
sslbacyhadls <sslbacyhadls@gmail.com>
2025-08-02 23:26:13 +0500

    added yaml support

---
+++ README.md
@@ -0,0 +1,16 @@
+# Скрипт для разворачивания x86_64 виртуалок из ямлика
+
+## Пример options.yaml файла
+
+'vm:
+   - name: void
+     ram: 4086
+     image: ./Images/void-live-x86_64-musl-20250202-base.iso
+     disk: void
+
+disks:
+   - name: void
+     format: qcow2
+     size: 20G
+     directory: ./disks'
+
--- init.sh
+++ init.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-DEFAULT_RAM_SIZE=2G
-DEFAULT_DISK_SIZE=20G
-
 if [[ $1 = help ]]; then
-	echo "Command: sudo init.sh install <image> <diskname>"
-	echo "         sudo init.sh boot <diskname>"
+	echo "Command: sudo init.sh install <vm_name>"
+	echo "         sudo init.sh boot <vm_name>"
 	exit 1
 fi
 
@@ -16,24 +13,42 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 
+VM_NAME=$2
+VM_RAM=$(yq [.] ./options.yaml | jq -r --arg VM_NAME "$VM_NAME" '.[0].vm | select(.[0].name ==  $VM_NAME ) | .[0].ram')
+VM_IMAGE=$(yq [.] ./options.yaml | jq -r  --arg VM_NAME "$VM_NAME" '.[0].vm | select(.[0].name ==  $VM_NAME ) | .[0].image')
+VM_DISK_NAME=$(yq [.] ./options.yaml | jq -r  --arg VM_NAME  "$VM_NAME" '.[0].vm | select(.[0].name == $VM_NAME) | .[0].disk')
 
-if [ ! -f $3 ]; then
-	echo "Creating disk $3"
-	exec qemu-img create -f qcow2 $3 $DEFAULT_DISK_SIZE
+VM_QEMU_BOOT_ARGUMENTS=$(yq [.] ./options.yaml | jq -r --arg VM_NAME "$VM_NAME" '.[0].vm | select(.[0].name == $VM_NAME) | .[0].additional_qemu_boot_arguments')
+VM_QEMU_INSTALL_ARGUMENTS=$(yq [.] ./options.yaml | jq -r  --arg VM_NAME "$VM_NAME" '.[0].vm | select(.[0].name == $VM_NAME) | .[0].additional_qumu_install_arguments')
+
+DISK_FORMAT=$(yq [.] ./options.yaml | jq  -r --arg VM_DISK_NAME "$VM_DISK_NAME" '.[0].disks | select(.[0].name == $VM_DISK_NAME) | .[0].format')
+DISK_SIZE=$(yq [.] ./options.yaml | jq  -r --arg VM_DISK_NAME "$VM_DISK_NAME" '.[0].disks | select(.[0].name == $VM_DISK_NAME) | .[0].size')
+DISK_DIRECTORY=$(yq [.] ./options.yaml | jq  -r --arg VM_DISK_NAME "$VM_DISK_NAME" '.[0].disks | select(.[0].name == $VM_DISK_NAME) | .[0].directory')
+
+DISK_PATH=$(echo ${DISK_DIRECTORY}/${VM_DISK_NAME}.${DISK_FORMAT})
+
+
+if [[ $1 =  install ]]; then
+	if [ -f $DISK_PATH ]; then
+		echo "Using existing disk"
+	else
+		echo "Creating disk $DISK_PATH"
+		qemu-img create -f $DISK_FORMAT $DISK_PATH $DISK_SIZE
+	fi
 fi
 
 if [[ $1 = boot ]]; then
-	qemu-system-x86_64 -drive file=$2 \
-							 -m $DEFAULT_RAM_SIZE \
+	qemu-system-x86_64 -drive file=$DISK_PATH \
+							 -m $VM_RAM \
 							 -net nic,model=e1000 \
 							 -enable-kvm \
 							 -boot c 
 fi
 
 if [[ $1 = install ]]; then
-	qemu-system-x86_64 -drive file=$3 \
-							 -cdrom $2 \
-							 -m $DEFAULT_RAM_SIZE \
+	qemu-system-x86_64 -drive file=$DISK_PATH \
+							 -cdrom $VM_IMAGE \
+							 -m $VM_RAM \
 							 -net nic,model=e1000 \
 							 -enable-kvm \
 							 -boot d
